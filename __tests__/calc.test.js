594a50767e797de0d904a4c5fc7482c095494fe3
Joseph Pearce <joseph@zotta.com>
2020-04-16 11:09:59 -0700

    add test for Date constructor with meaningful fail

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -1,11 +1,26 @@
+import { DateInput } from "./../src/calc.js";
 import { Date } from "./../src/calc.js";
 
-describe ("Date", () => {
+describe ("DateInput", () => {
   test("should correctly create a date object with a month, day, and year attribute", () => {
-    var date = new Date(11, 5, 1990);
+    let date = new DateInput(11, 5, 1990);
     expect(date.month).toEqual(11);
     expect(date.day).toEqual(5);
     expect(date.year).toEqual(1990);
   });
 });
 
+describe ("Date", () => {
+  test("should correctly convert Date Input info to native Date object", () => {
+    let date = new DateInput(11, 5, 1990);
+    let convertMonth = date.month - 1;
+    let newDate = new Date (date.year, convertMonth, date.day);
+    expect(newDate.month).toEqual(10);
+    expect(newDate.day).toEqual(5);
+    expect(newDate.year).toEqual(1990);
+  })
+})
+
+// var d = Date.parse("March 21, 2012");
+
+// let birthday = new Date(1995, 11, 17) 
--- src/calc.js
+++ src/calc.js
@@ -1,10 +1,12 @@
 // business logic - Export function
 
-export function Date(month, day, year) {
+export function DateInput (month, day, year) {
   this.month = month;
   this.day = day;
   this.year = year;
 }
 
+export function Date (year, month, day) {
 
+}
 
